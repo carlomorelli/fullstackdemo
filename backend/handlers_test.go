c01737bf7629053b5d3d25c24d9dc657b24a960e
Carlo Morelli <carlomorelli@gmail.com>
2018-06-02 20:13:02 +0200

    Add handler unit tests and Dockerfile

--- backend/handlers_test.go
+++ backend/handlers_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,42 +17,76 @@ const (
 )
 
 func TestPingHandler_ShouldReturnAnswer(t *testing.T) {
-
 	req, err := http.NewRequest("GET", "/ping", nil)
-	if err != nil {
-		assert.Fail(t, "Http request failed")
-	}
+	assert.Nil(t, err, "Http request failed")
 	recorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(PingHandler)
-	handler.ServeHTTP(recorder, req)
-
+	http.HandlerFunc(PingHandler).ServeHTTP(recorder, req)
 	assert.Equal(t, plaintext, recorder.HeaderMap.Get("Content-type"))
 	assert.Equal(t, http.StatusOK, recorder.Code)
 	assert.Equal(t, "pong", string(recorder.Body.Bytes()))
-
 }
 
-func TestGetUserHandler_WithValidLoginInput_ShouldReturnValidUserDetails(t *testing.T) {
-
-	req, err := http.NewRequest("GET", "/users/1", nil)
-	if err != nil {
-		assert.Fail(t, "Http request failed")
-	}
+func TestLoginHandler_WithValidBody_ShouldReturnToken(t *testing.T) {
+	login := LoginData{Email: "demo@empatica.com", Password: "passw0rd"}
+	loginjson, _ := json.Marshal(login)
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(loginjson))
+	assert.Nil(t, err, "Http request failed")
 	recorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(GetUserHandler)
-	handler.ServeHTTP(recorder, req)
-
+	http.HandlerFunc(LoginHandler).ServeHTTP(recorder, req)
 	assert.Equal(t, plaintext, recorder.HeaderMap.Get("Content-type"))
 	assert.Equal(t, http.StatusOK, recorder.Code)
-	assert.Equal(t, "pong", string(recorder.Body.Bytes()))
-
+	var ua *UserAccount
+	err2 := json.Unmarshal(recorder.Body.Bytes(), &ua)
+	assert.Nil(t, err2)
+	assert.Equal(t, "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9", ua.Token)
 }
 
-/* //mock function
-func getUser(userID int64) *User {
-
-	args := m.Called(userID)
-	return args.User
-
+func TestLoginHandler_WithInvalidUser_Should403Forbidden(t *testing.T) {
+	login := LoginData{Email: "invaliduser", Password: "passw0rd"}
+	loginjson, _ := json.Marshal(login)
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(loginjson))
+	assert.Nil(t, err, "Http request failed")
+	recorder := httptest.NewRecorder()
+	http.HandlerFunc(LoginHandler).ServeHTTP(recorder, req)
+	assert.Equal(t, http.StatusForbidden, recorder.Code)
+	assert.Equal(t, "invalid user", string(recorder.Body.Bytes()))
 }
-*/
+
+func TestLoginHandler_WithInvalidBody_ShouldReturn400BadRequest(t *testing.T) {
+	req, err := http.NewRequest("POST", "/login", strings.NewReader("invalidjson"))
+	assert.Nil(t, err, "Http request failed")
+	recorder := httptest.NewRecorder()
+	http.HandlerFunc(LoginHandler).ServeHTTP(recorder, req)
+	assert.Equal(t, http.StatusBadRequest, recorder.Code)
+	assert.Equal(t, "invalid login data", string(recorder.Body.Bytes()))
+}
+
+func TestGetUserHandler_WithExistingId_ShouldReturnUserDetails(t *testing.T) {
+	req, err := http.NewRequest("GET", "/users/{userId}", nil)
+	req = mux.SetURLVars(req, map[string]string{"userId": "1"})
+	assert.Nil(t, err, "Http request failed")
+	recorder := httptest.NewRecorder()
+	http.HandlerFunc(GetUserHandler).ServeHTTP(recorder, req)
+	assert.Equal(t, plaintext, recorder.HeaderMap.Get("Content-type"))
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	assert.Equal(t, "{\"id\":1,\"email\":\"demo@empatica.com\",\"firstName\":\"John\",\"lastName\":\"\",\"age\":13}\n", string(recorder.Body.Bytes()))
+}
+
+func TestGetUserHandler_WithUnexistingId_ShouldReturn404NotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/users/{userId}", nil)
+	req = mux.SetURLVars(req, map[string]string{"userId": "47"})
+	assert.Nil(t, err, "Http request failed")
+	recorder := httptest.NewRecorder()
+	http.HandlerFunc(GetUserHandler).ServeHTTP(recorder, req)
+	assert.Equal(t, http.StatusNotFound, recorder.Code)
+	assert.Equal(t, "missing user", string(recorder.Body.Bytes()))
+}
+
+func TestGetUserHandler_WithMalformedUrl_ShouldReturn400BadRequest(t *testing.T) {
+	req, err := http.NewRequest("GET", "/users/{userId}", nil)
+	req = mux.SetURLVars(req, map[string]string{"userId": "invalidint"})
+	assert.Nil(t, err, "Http request failed")
+	recorder := httptest.NewRecorder()
+	http.HandlerFunc(GetUserHandler).ServeHTTP(recorder, req)
+	assert.Equal(t, http.StatusBadRequest, recorder.Code)
+}
