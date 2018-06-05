44cd8805683a6513723533dd5e497dab2761ee3d
Carlo Morelli <carlomorelli@gmail.com>
2018-06-05 20:59:54 +0200

    Add docker container for frontend

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
-# build backend container (runs unit tests as well)
+# build backend container
 docker build -t backend-image ./backend
 
 # deploy backend
 docker stop backend || true
 docker rm backend || true
-docker run -d -p 8080:9000 --name backend backend-image
+docker run -d -p 9000:9000 --name backend backend-image
 
 # run blackbox tests
 cd backend
-BASEURL=localhost:8080 go test -v -tags blackbox
+BASEURL=localhost:9000 go test -v -tags blackbox
+cd ..
+
+# build frontend container
+docker build -t frontend-image ./frontend
+
+# deploy frontend
+docker stop frontend || true
+docker rm frontend || true
+docker run -d -p 4200:4200 --name frontend frontend-image
+
+# run jasmine tests
+cd frontend
+#TODO
+cd ..
